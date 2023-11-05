980a8742aaa524ec525909204fd725b15af548a1
Davidkothe3 <khotedevid@gmail.com>
2023-11-05 22:10:32 +0530

    c#

---
+++ C#/and a method getdata() with no parameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace and_a_method_getdata___with_no_parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C#/another example of copy constructor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace another_example_of_copy_constructor
+{  {
+    class employee
+    {
+        private string name;
+        private int age;
+        public employee(employee emp)  
+        {
+            name = emp.name;
+            age = emp.age;
+        }
+        public employee(string name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Details     
+        {
+            get
+            {
+                return " The age of " + name + " is " + age.ToString();
+            }
+        }
+    }
+    class empdetail
+    {
+
+        internal class Program
+        {
+            static void Main(string[] args)
+
+            {
+                employee emp1 = new employee("Vithal", 23);  // Create a new employee object.
+                employee emp2 = new employee(emp1);          // here is emp1 details is copied to emp2.
+                Console.WriteLine(emp2.Details);
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
