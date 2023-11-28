645ce0dcdaab9fc380e7241786679eafa3af38e7
Davidkothe3 <khotedevid@gmail.com>
2023-11-28 23:13:27 +0530

    c#

---
+++ C#/distionary collection windows 1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace distionary_collection_windows_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, string> domains = new Dictionary<string, string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            domains.Add(textBox1.Text, textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+            MessageBox.Show("added in dictionary");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> keys = new List<string>(domains.Keys);
+            foreach (string key in keys)
+            {
+                sb.Append("keys in dictionay " + key + "\n");
+            }
+
+            label3.Text = sb.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> keys = new List<string>(domains.Values);
+            foreach (string key in keys)
+            {
+                sb.Append("values in dictionay " + key + "\n");
+            }
+            label4.Text = sb.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<string, string> kvp in domains)
+            {
+                sb.Append("key " + kvp.Key + " value " + kvp.Value + "\n");
+            }
+            label4.Text = sb.ToString();
+
+        }
+    }
+}
