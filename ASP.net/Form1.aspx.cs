27fa066d2a9a9a2ae214af459759cc9734140473
Davidkothe3 <khotedevid@gmail.com>
2024-01-18 22:04:13 +0530

    asp

---
+++ ASP.net/Form1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication_18jan
+{
+    public partial class Form1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                stype = "student";
+                TextBox1.Text = "1000";
+            }
+        }
+       static string stype = "student";
+        
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+          if(RadioButton2.Checked)
+          {
+                stype = "student";
+                TextBox1.Text = "3000";
+                TextBox2.Text = "0";
+                TextBox3.Text = "0";
+          }
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked)
+            {
+
+            }
+        }
+    }
+}
---
+++ ASP.net/Form1_12jan.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace product_Store_12jan
+{
+    public partial class Form1 : Form
+    {
+        string strcon = "server =.\\SQLEXPRESS; integrated security = true; database = TAbleProductCategory_11jan";
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getcategory();
+            getproduct();   
+        }
+        public void getcategory()
+        {
+            try
+            {
+                con = new SqlConnection(strcon);
+                da = new SqlDataAdapter("select * from TableProductCategory order by Product_Category_ID", con);
+                ds = new DataSet();
+                da.Fill(ds, "tpc");
+                comboBox1.DataSource = ds.Tables["tcp"];
+                comboBox1.DisplayMember = "Product_Type_Name";
+                comboBox1.ValueMember = "Product_Category_ID";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public void getproduct()
+        {
+
+            if(comboBox1.SelectedValue != null && comboBox1.SelectedValue.GetType() != typeof(System.Data.DataRowView))
+            {               
+                    con = new SqlConnection(strcon);
+                    da = new SqlDataAdapter("select * from Tableproduct where Product_Category_ID = @pci", con);
+                    da.SelectCommand.Parameters.AddWithValue("@pci", comboBox1.SelectedValue);
+                    ds = new DataSet();
+                    da.Fill(ds,"product");
+                    comboBox2.DataSource = ds.Tables["product"];
+                    comboBox2.DisplayMember = "Product_Name";
+                    comboBox2.ValueMember = "ProductID";    
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            getproduct();
+        }
+    }
+}
